87bc537857133d4c8ab05e807ff3654fc08b245d
Julian Codespoti <julian.codespoti@myob.com>
2024-09-03 18:35:48 +1000

    change to version 7

--- CreditTask/obj/Debug/net7.0/CreditTask.AssemblyInfo.cs
+++ CreditTask/obj/Debug/net7.0/CreditTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b305e82b524f8ed25530e0bfe15a9e316d08ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
